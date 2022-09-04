c8bd3bba8d40478685d6a8989794927180371515
rian000614 <iyanirawan71@gmail.com>
2022-09-04 21:44:55 +0700

    Finishing print header

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,19 @@
 class App {
+    fun printHeader(){
+        println("""
+            ===================
+            Gunting Batu Kertas
+            ===================
+        """.trimIndent())
+    }
+
+    fun run(){
+        printHeader()
+    }
     companion object{
         @JvmStatic
         fun main(args: Array<String>) {
+            App().run()
 
         }
     }
